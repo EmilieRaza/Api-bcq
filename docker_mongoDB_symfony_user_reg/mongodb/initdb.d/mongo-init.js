e308fd6d02b35e66262ad3b034457e5e14733d5f
EmilieRaza <razafiemilie@hotmail.fr>
2021-04-22 09:44:25 +0200

    ---

--- docker_mongoDB_symfony_user_reg/mongodb/initdb.d/mongo-init.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB('admin')
-db.auth('root', 'root')
-db = db.getSiblingDB('symfony')
-
-db.createUser({
-  user: "symfony",
-  pwd: "symfony",
-  roles: [{role: "readWrite", db: "symfony"}]
-});
